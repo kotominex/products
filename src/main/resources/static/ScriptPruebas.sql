269e9f46f5cee3bc37e7e3a5b6a4b806962453ad
salvearp <desarrollador.steven@ada.co>
2026-06-05 04:51:36 -0500

    se agrega sql con informacion de prueba

---
+++ src/main/resources/static/ScriptPruebas.sql
@@ -0,0 +1,22 @@
+About of me
+
+I am a systems technologist with more than five years of experience in software development. 
+I hold ISO 27001 certification from 2022. I like the idea of ​​immigrating to Canada because it's a large and beautiful country 
+with a cool climate that fascinates me. 
+I hope to work and spend time with my family 
+to provide a high-quality education for my children 
+while also being able to travel around the country.
+
+About of my family
+
+My children are 2 years old, the oldest, and 7 months old.
+I'm currently studying conversational English, and my written English is good because my work is 100% in English.
+My wife is currently studying English, and my children are just learning to speak Spanish, so I think it might be easier for us to adapt.
+
+
+
+Que hice en ADA
+
+I was a senior developer, leading four development teams (quoting, contact center, data science, and self-service). 
+Additionally, I developed a suite of microservices across the board to ensure data security, consistency, and uniformity in communication, 
+so that Active Directory, the ORM, and our other applications would all have the same user information.
